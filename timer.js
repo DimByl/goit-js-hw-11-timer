b6f8c1f1e7311dca19e44eb1fa395aeef3cf5789
Dmytro Bykanov <75586266+DimByl@users.noreply.github.com>
2021-07-17 12:49:28 +0300

    up2

--- timer.js
+++ timer.js
@@ -21,7 +21,11 @@ export default function CountdownTimer({ selector, targetDate }) {
 
     window.onload = function () {
         setInterval(() => {
-        const time = targetDate - Date.now();
+            const time = targetDate - Date.now();
+             if (time < 0) {
+                clearInterval(window.onload)
+                return
+            };
         const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
         const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
         const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
